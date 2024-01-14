a13ba9ab471bdae297d0d94ec8935cbc4dfa2a7d
nineleaps <indranil.bhuin11092001@gmail.com>
2024-01-14 12:44:30 +0530

    chore: install necessary dependencies and add constants

---
+++ assets/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  charcoalBlack: '#1B262E',
+  deepBlack: '#354349',
+  steelGray: '#606D76',
+  lightGray: '#A9B4BC',
+  smokeWhite: '#C5CDD2',
+  pureWhite: '#E7ECF0',
+  navyBlue: '#2A4BA0',
+  midnightBlue: '#153075',
+  sunflowerYellow: '#F9B023',
+  goldenYellow: '#FFC83A',
+};
+
